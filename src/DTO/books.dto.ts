20bd339dfae182729fc34fed6d678063abee60d8
luizguilhermejrm <luizguilhermejrm@hotmail.com>
2023-01-15 21:14:05 -0300

    feat: validation dtos

--- src/DTO/books.dto.ts
+++ src/DTO/books.dto.ts
@@ -1,4 +1,6 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+import { IsNotEmpty, IsString, MaxLength, MinLength, IsNumber, IsPositive, ArrayMinSize, ValidateNested } from "class-validator";
+import { Type } from 'class-transformer';
+import { AuthorDTO } from "./author.dto";
 
 export class BookDTO {
 
@@ -7,9 +9,32 @@ export class BookDTO {
     @MinLength(2)
     @MaxLength(100)
     readonly name: string;
-    readonly author: string[];
+
+    @IsNotEmpty()
+    @Type(() => AuthorDTO)
+    @ArrayMinSize(1)
+    @ValidateNested()
+    readonly author: AuthorDTO[];
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(2)
+    @MaxLength(100)
     readonly language: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
     readonly releaseYear: number;
+    
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(2)
+    @MaxLength(100)
     readonly publisher: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
     readonly pages: number;
 }
